1146a9848e8eb7eb65a2d4a8b1be607ad13d5eb7
i <ibelyalov@yandex.ru>
2020-01-21 01:18:13 +0300

    Change test error messages

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -94,11 +94,11 @@ class SeleniumTestCase(unittest.TestCase):
 
         # navigate to the user's profile page
         self.client.find_element_by_link_text('Profile').click()
-        self.assertTrue('<h1>bob</h1>' in self.client.page_source)
+        self.assertTrue('<h1>bob</h1>' in self.client.page_source, 'Нет имени юзера')
 
 
     def test_user_page(self):
         # navigate to home page
         self.client.get('http://localhost:5000/user/john')
         self.assertTrue(re.search('Hello,\s+Stranger!',
-                                  self.client.page_source))
+                                  self.client.page_source), 'Нет приветственного текста')
